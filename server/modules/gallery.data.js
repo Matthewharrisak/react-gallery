1cb47e8e417f9076fd24373d7c06b58769101714
Matthew Harris <matthewharrisak@gmail.com>
2020-10-25 09:38:37 -0500

    added more styling and update gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,10 @@
 const galleryItems = [
-    { id: 1, path: './images/SolCone.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: './images/veganSandwhich.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: './images/emmaMattWedding.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: './images/edKipMatt.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 5, path: './images/chipsCouch.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 6, path: './images/augustMattCroc.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: './images/SolCone.jpeg', description: 'My Friend Beccas dog, Sol! She got in a fight with another dog and needed a cone.' ,likes: 0 },
+    { id: 2, path: './images/veganSandwhich.jpeg', description: 'Vegan Sandwhich from the internet.', likes: 0 },
+    { id: 3, path: './images/emmaMattWedding.jpeg', description: 'Emma and I drinkning Lacroix at our Wedding.', likes: 0 },
+    { id: 4, path: './images/edKipMatt.jpeg', description: 'Ed, Kip and I hiking in Alaska!', likes: 0 },
+    { id: 5, path: './images/chipsCouch.jpeg', description: 'My cat Dr Chips, sitting on the couch! ', likes: 0 },
+    { id: 6, path: './images/augustMattCroc.jpeg', description: 'freshly tattood and wearing crocs with my friend August.', likes: 0 },
     
 
     
